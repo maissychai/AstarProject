fc4b5a7421831bddde23bed2be52e9cd5cd34a28
ridhogillang <Ridhogillangf@gmail.com>
2018-09-21 23:29:40 +0700

    backendatlng

--- app.js
+++ app.js
@@ -1,41 +1,102 @@
-var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var index = require('./routes/index');
 var app = express();
+var cookieSession = require('cookie-session')
+var mysql = require('mysql');
+var flash = require('connect-flash');
+var cors = require('cors');
 
+//require('./config/passport')(passport);
+require('dotenv').config();
+
+if (app.get('env') == 'production') {
+  require('dotenv').config();
+}
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/public/views'));
+app.set('trust proxy', true);
+app.use('*', cors());
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.set('view engine', '.hbs');
+app.set('view options', {
+  layout: 'index'
 });
 
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.alert = req.flash('alert');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+// Handlebars default config
+const hbs = require('hbs');
+const fs = require('fs');
+
+const partialsDir = __dirname + '/public/views/partials';
+
+const filenames = fs.readdirSync(partialsDir);
+
+filenames.forEach(function (filename) {
+  const matches = /^([^.]+).hbs$/.exec(filename);
+  if (!matches) {
+    return;
+  }
+  const name = matches[1];
+  const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+  hbs.registerPartial(name, template);
+});
+
+hbs.registerHelper('json', function (context) {
+  return JSON.stringify(context, null, 2);
+});
+
 
 module.exports = app;
